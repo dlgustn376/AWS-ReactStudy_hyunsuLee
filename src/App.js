9e49477333c3b37b60c53dc3d92ab5d3a9897e8a
dlgustn3768 <dlgustn3768@naver.com>
2023-03-24 17:53:33 +0900

    users table create

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import InputSample from "./Pages/InputSample/InputSample";
 import MainAside from "./component/Aside/MainAside/MainAside";
 import { Global } from "@emotion/react";
 import { reset } from "./styles/Global/reset";
+import UserList from "./Pages/UserList/UserList";
 
 function Test1(){
     return(<h1>Test1</h1>);
@@ -21,6 +22,7 @@ function App() {
         <Route path="/t1" Component={Test1}/> 
         <Route path="/t2" Component={Test2}/> 
         <Route path="/sample/input/1" Component={InputSample}/> 
+        <Route path="/users" Component={UserList}/> 
       </Routes>
     </>    
   );
--- src/component/Aside/MainAside/MainAside.js
+++ src/component/Aside/MainAside/MainAside.js
@@ -3,26 +3,37 @@ import React from 'react';
 import Icon from "awesome-react-icons/lib/cjs/Icon";
 import { Navigation } from "react-minimal-side-navigation/lib";
 import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+import { useNavigate } from 'react-router-dom';
+
 import { GrTest } from 'react-icons/gr';
 import { HiHome } from 'react-icons/hi';
+import { IoIosCalendar } from 'react-icons/io';
 import { BsCardChecklist } from 'react-icons/bs';
 import { BiListCheck } from 'react-icons/bi';
+import { FaUser } from 'react-icons/fa';
 
 import * as S from './Style'
 
 const MainAside = () => {
+    const navigate = useNavigate();
+
     return (
         <aside css={S.style}>
             <Navigation
             activeItemId="/"
             onSelect={({itemId}) => {
-                console.log(itemId);
+                navigate(itemId);
             }}
             items={[
                 {
+                    title: 'Home',
+                    itemId: '/',
+                    elemBefore: () => <HiHome />
+                },
+                {
                     title: 'T1',
                     itemId: '/t1',
-                    elemBefore: () => <HiHome />
+                    elemBefore: () => <IoIosCalendar />
                 },
                 {
                     title: 'T2',
@@ -31,13 +42,25 @@ const MainAside = () => {
                 },
                 {
                     title: 'Sample',
-                    itemId: '/sample',
+                    itemId: '/sample/input/1',
                     elemBefore: () => <BsCardChecklist />,
                     subNav: [
                         {
                             title: 'input',
                             itemId: '/sample/input/1',
                             elemBefore: () => <BiListCheck />    
+                        }
+                    ]
+                },
+                {
+                    title: 'List',
+                    itemId: '/users',
+                    elemBefore: () => <BsCardChecklist />,
+                    subNav: [
+                        {
+                            title: '사용자 전체 조회',
+                            itemId: '/sample/input/1',
+                            elemBefore: () => <FaUser />    
                         }
                     ]
                 },
